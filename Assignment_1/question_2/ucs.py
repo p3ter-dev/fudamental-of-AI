d41df6115c05b4a3c000d533568d313273a9b979
peter <peterkinfe548@gmail.com>
2025-04-27 22:40:51 +0300

    logic using uniform cost search for question 2

--- Assignment_1/question_2/ucs.py
+++ Assignment_1/question_2/ucs.py
@@ -3,23 +3,21 @@ from graph_conversion import graph
 def uniform_cost_search(graph, start, goal):
     priority_queue = []
     heapq.heappush(priority_queue, (0, start, [start]))
-    visited = set()
-
+    cost_so_far = {start: 0}
     while priority_queue:
         cost, node, path = heapq.heappop(priority_queue)
 
         if node == goal:
             return path, cost
 
-        if node not in visited:
-            visited.add(node)
-
-            for neighbor, edge_cost in graph.get(node, []):
-                if neighbor not in visited:
-                    heapq.heappush(priority_queue, (cost + edge_cost, neighbor, path + [neighbor]))
+        for neighbor, edge_cost in graph.get(node, []):
+            new_cost = cost + edge_cost
+            if neighbor not in cost_so_far or new_cost < cost_so_far[neighbor]:
+                cost_so_far[neighbor] = new_cost
+                heapq.heappush(priority_queue, (cost + edge_cost, neighbor, path + [neighbor]))
 
     return None, float('inf')
 
-path, total_cost = uniform_cost_search(graph, 'Addis Ababa', 'Adama')
+path, total_cost = uniform_cost_search(graph, 'Addis Ababa', 'Lalibela')
 print("Path:", path)
 print("Total Cost:", total_cost)
