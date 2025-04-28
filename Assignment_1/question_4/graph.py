c3239d5144c5cc429e339f59a38c0c38f5caf13e
peter <peterkinfe548@gmail.com>
2025-04-28 11:00:34 +0300

    graph for adversarial search

---
+++ Assignment_1/question_4/graph.py
@@ -0,0 +1,12 @@
+graph = {
+    'Addis Ababa': ['Ambo', 'Adama', 'Buta Jira'],
+    'Ambo': ['Gedo', 'Nekemte'],
+    'Gedo': ['Shambu', 'Fincha'],
+    'Nekemte': ['Gimbi', 'Limu'],
+    'Worabe': ['Hossana', 'Durame'],
+    'Wolkite': ['Bench Maji', 'Tepi'],
+    'Adama': ['Dire Dawa', 'Mojo'],
+    'Mojo': ['Dilla', 'Kaffa'],
+    'Buta Jira': ['Worabe', 'Wolkite'],
+    'Dire Dawa': ['Harar', 'Chiro'],
+}
