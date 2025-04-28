2b9ebfc2cce8e490b0f3bb7a2e47431401ff374f
peter <peterkinfe548@gmail.com>
2025-04-28 11:03:50 +0300

    utitlities for adversarial search

---
+++ Assignment_1/question_4/utilities.py
@@ -0,0 +1,14 @@
+utilities = {
+    'Shambu': 4,
+    'Fincha': 5,
+    'Gimbi': 8,
+    'Limu': 8,
+    'Harar': 10,
+    'Chiro': 6,
+    'Dilla': 9,
+    'Kaffa': 7,
+    'Hossana': 6,
+    'Durame': 5,
+    'Bench Naji': 5,
+    'Tepi': 6
+}
