0b5330304db5bb7244fdb08e7119a0324fe2d382
peter <peterkinfe548@gmail.com>
2025-04-28 11:32:15 +0300

    customized uniform cost search for finding many goals

--- Assignment_1/question_2/cucs.py
+++ Assignment_1/question_2/cucs.py
@@ -1,5 +1,5 @@
-from Assignment_1.question_2.ucs import uniform_cost_search
-
+from graph_conversion import graph
+from ucs import uniform_cost_search
 def customized_uniform_cost_search(graph, start, goals):
     current = start
     unvisted_goals = set(goals)
@@ -21,9 +21,15 @@ def customized_uniform_cost_search(graph, start, goals):
         if not next_goal:
             break
 
-        total_path += path_to_next[1:]
+        total_path += path_to_next
         total_cost += min_cost
         current = next_goal
         unvisted_goals.remove(next_goal)
 
     return total_path, total_cost
+
+path, cost = customized_uniform_cost_search(graph, 'Addis Ababa',
+ ['Axum', 'Gonder', 'Lalibela', 'Babile', 'Jimma', 'Bale', 'Sof Oumer', 'Arba Minch'])
+
+print("Path:", path)
+print("Total Cost:", cost)
